174eea52d7fafbf98571692f36723b9bc59c1453
Felipe Freitas <feelipe.freitas97@gmail.com>
2021-12-18 17:19:19 -0300

    Criação da estrutura base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { IPostCommentResponse } from './shared/interfaces/responses/post-comment.response.interface';
+import { IPostResponse } from './shared/interfaces/responses/post.response.interface';
+import { PostCommentsService } from './shared/services/post-comments.service';
+import { PostsService } from './shared/services/posts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'curso-angular-testes-components';
+export class AppComponent implements OnInit {
+
+  posts: IPostResponse[] = [];
+  postComments: IPostCommentResponse[] = [];
+
+  constructor(
+    private _postsService: PostsService,
+    private _postCommentsService: PostCommentsService
+  ) { }
+
+  ngOnInit(): void {
+    this.returnPostsList();    
+  }
+
+  loadPostComments(postId: number) {
+    this._postCommentsService.getPostComments(postId).subscribe(commentsResponse => {
+      this.postComments = commentsResponse;
+    });
+  }
+
+  returnPostsList() {
+    this._postsService.getPosts().subscribe(postsResponse => {
+      this.posts = postsResponse;
+    });
+  }
 }
